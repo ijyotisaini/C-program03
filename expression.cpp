6dec38fdbeee60046dec656cfc52b296ed3a6ae3
Jyoti Saini <136901655+ijyotisaini@users.noreply.github.com>
2025-09-29 22:46:56 +0530

    Add files via upload

---
+++ expression.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+int a,b,c,d,e,f,g;
+
+cout<<"Enter the value of a:";
+cin>>a;
+
+cout<<"Enter the value of b:";
+cin>>b;
+
+cout<<"Enter the value of c:";
+cin>>c;
+
+cout<<"Enter the value of d:";
+cin>>d;
+
+cout<<"Enter the value of e:";
+cin>>e;
+cout<<"Enter the value of f:";
+cin>>f;
+
+cout<<"Enter the value of g:";
+cin>>g;
+
+int value= ((a+b/c*d-e)*(f-g));
+cout<<value;
+
+	return 0;
+}
